1e83c4f3e772ef153f599bf301ab9bef1bb4a826
Anya1014CN <han.cf@foxmail.com>
2025-05-11 21:30:42 +0800

    智慧陕理: 发布版本 1.12.35

--- lib/globalvars.dart
+++ lib/globalvars.dart
@@ -4,9 +4,9 @@ import 'package:dio/dio.dart';
 
 class GlobalVars {
   //当前版本号
-  static String versionCodeString = '1.12.32';
-  static int versionCodeInt = 1012032;
-  static String versionReleaseDate = '2025-05-04';
+  static String versionCodeString = '1.12.35';
+  static int versionCodeInt = 1012035;
+  static String versionReleaseDate = '2025-05-09';
 
   //友盟统计相关
   static bool isPrivacyAgreed = false; // 是否同意隐私政策
